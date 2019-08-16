8137af412e37a49ef935fc29c6ed0452ff8354e0
Jacob Vu <vuthelinh@gmail.com>
2019-08-16 14:32:28 +0700

    ChromeOptions with serenity.properties

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # web-pageobject-junit
 Sample project for web automated testing
+
+```
+>.\chromedriver.exe --version
+ChromeDriver 75.0.3770.140 (2d9f97485c7b07dc18a74666574f19176731995c-refs/branch-heads/3770@{#1155})
+
+Google Chrome: Version 75.0.3770.142 (Official Build) (64-bit)
+```
+
+Tùy chỉnh cấu hình ChromeDriver sử dụng `serenity.properties`
+
+
--- src/test/java/vn/amabuy/features/register/WhenRegisterNewAccount.java
+++ src/test/java/vn/amabuy/features/register/WhenRegisterNewAccount.java
@@ -16,23 +16,23 @@ import vn.amabuy.steps.LoginSteps;
 import vn.amabuy.steps.RegisterSteps;
 
 @RunWith(SerenityParameterizedRunner.class)
-@UseTestDataFrom(value="testdata/accounts/user-info.csv") 
+@UseTestDataFrom(value = "testdata/accounts/user-info.csv")
 public class WhenRegisterNewAccount {
-	
+
 	@Nullable
 	private String fullName;
-	
+
 	@Nullable
 	private String email;
-	
+
 	public void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	@Managed
 	WebDriver driver;
 
@@ -41,18 +41,17 @@ public class WhenRegisterNewAccount {
 
 	@Steps
 	LoginSteps loginSteps;
-	
+
 	@Steps
 	RegisterSteps registerSteps;
-	
+
 	String errMsg = "Mật khẩu xác nhận không chính xác";
 
 	@Test
 	@WithTag("register")
 	public void a_register_new_account_with_incorrect_password() {
-		
-		Account infor = Account
-				.named(fullName)
+
+		Account infor = Account.named(fullName)
 				.withEmail(email)
 				.withPhone("0984303104")
 				.withPassword("teaching webdriver online")
@@ -60,13 +59,13 @@ public class WhenRegisterNewAccount {
 				.build();
 
 		jacob.visit_application();
-	//	jacob.click_on_login_link();
+		jacob.click_on_login_link();
 
-	//	loginSteps.click_on_register_link();
+		loginSteps.click_on_register_link();
 
-	//	registerSteps.register_new_account_with(infor);
-	//	registerSteps.should_see_warning_error_message_correct(errMsg);
-		
+		registerSteps.register_new_account_with(infor);
+		registerSteps.should_see_warning_error_message_correct(errMsg);
+
 	}
 
 }
